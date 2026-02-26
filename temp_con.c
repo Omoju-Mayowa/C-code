5bc8458149ba82a4420062fa7d4cfcb6890792dc
Omoju-Mayowa <omojumayowa@gmail.com>
2026-02-26 10:18:09 +0100

    Pushing temperature converter

--- temp_con.c
+++ temp_con.c
@@ -2,8 +2,10 @@
 #include <ctype.h>
 
 
-int main() {
+int main() { 
     char choice = '\0';
+    float celsius = 0.00;
+    float fahrenheit = 0.00;
 
     printf("Temperature COnversion Program\n");
     printf("C. Celsius to Fahrenehit\n");
@@ -16,9 +18,20 @@ int main() {
 
 
     if(choice == 'c') {
-        printf("Enter the teperature in Celcius");
+        printf("Enter the teperature in Celcius: ");
+        scanf("%f", &celsius);
+
+        fahrenheit = ((celsius * 9/5) + 32);
+        // printf("%7.1f\n", celsius);
+        printf("%.1fºC in Fahrenheit is %.1fºF\n", celsius, fahrenheit);
+        
     }   else if(choice == 'f') {
-        printf("Enter the teperature in Celcius");
+        printf("Enter the teperature in Fahrenheit: ");
+        scanf("%f", &fahrenheit);
+
+        celsius = ((fahrenheit - 32) * 5/9);
+
+        printf("%.1fºF in Celsuis is %.1fºC", fahrenheit, celsius);
     }   else {
         printf("Invalid choice! Please select C or F.");
         scanf(" %c", &choice);
@@ -26,5 +39,5 @@ int main() {
 
 
 
-
 }
+
