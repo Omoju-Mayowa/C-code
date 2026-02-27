cad3c10b8f8862e2525538ace2695417a44237e9
Omoju-Mayowa <omojumayowa@gmail.com>
2026-02-27 21:15:43 +0100

    Made var.c file in basics

---
+++ src/control_flow/build/var.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+// Defining constant
+#define PI 3.14157
+
+int main() {
+    // Int Variable
+    int a = 5;
+    // Float Variable
+    float b = 6.24;
+    // Single Char Variable
+    char c = 'c';
+    // Multiple Char Variable
+    char d[] = "words";
+
+
+    // Defining constant
+    const float PI2 = 3.14287;
+
+    // Printing Integer
+    printf("Int: %d\n", a);
+
+    // Printing Float
+    printf("Float: %f\n", b);
+
+    // Printing Single Char
+    printf("Single Char: %c\n", c);
+
+    // Printing Multi Char
+    printf("Mutiple Char: %s\n", d);
+
+
+    // Printing the constans
+    printf("First Constant: %f\n", PI);
+    printf("Second Constant: %f\n", PI2);
+
+}
